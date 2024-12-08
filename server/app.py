5d080e3022a7c7473c82d368472129efe71e46e6
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-12-08 18:10:59 +0300

    errors

--- server/app.py
+++ server/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate
-from flask import Flask, request, make_response
 from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -19,11 +21,68 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.route('/')
+def home():
+    return ''
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+class RestaurantById(Resource):
+    def get(self, id):
+        try:
+            restaurant = Restaurant.query.get_or_404(id)
+            return restaurant.to_dict(), 200
+        except Exception as e:
+            return {"error": "Restaurant not found"}, 404
+
+    def delete(self, id):
+        try:
+            restaurant = Restaurant.query.get_or_404(id)
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        except Exception as e:
+            return {"errors": str(e)}, 404
 
 
-if __name__ == "__main__":
+api.add_resource(RestaurantById, "/restaurants/<int:id>")
+
+class Restaurants(Resource):
+    def get(self):
+        try:
+            restaurants = Restaurant.query.all()
+            return [restaurant.to_dict(only=("id", "name", "address")) for restaurant in restaurants], 200
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+api.add_resource(Restaurants, "/restaurants")
+
+class Pizzas(Resource):
+    def get(self):
+        try:
+            pizzas = Pizza.query.all()
+            return [pizza.to_dict(only=("id", "name", "ingredients")) for pizza in pizzas], 200
+        except Exception as e:
+            return {"error": str(e)}, 400
+
+api.add_resource(Pizzas, "/pizzas")
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_restaurant_obj = RestaurantPizza(
+                price=data["price"],
+                pizza_id=data["pizza_id"],
+                restaurant_id=data["restaurant_id"]
+            )
+            db.session.add(new_restaurant_obj)
+            db.session.commit()
+            return make_response(new_restaurant_obj.to_dict(), 201)
+        except ValueError as e:
+            return {"errors": ["validation errors"]}, 400
+        except Exception as e:
+            return {"errors": str(e)}, 400
+
+api.add_resource(RestaurantPizzas, "/restaurant_pizzas")
+
+if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,11 +21,13 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates= "restaurant", cascade="all, delete-orphan")
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
-        return f"<Restaurant {self.name}>"
+        return f"<Restaurant{self.name}>"
 
 
 class Pizza(db.Model, SerializerMixin):
@@ -36,8 +38,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza", cascade="all, delete-orphan")
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +52,24 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    
     # add relationships
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
+    
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
 
     # add serialization rules
+    serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas",)
 
     # add validation
+    @validates("price")
+    def validate_price(self, key, value):
+        if not 1 <= value <= 30:
+            raise ValueError("Price must be in between $1-30")
+        return value
+
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
